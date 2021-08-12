44940d43173bdd16a5d93e5735f2628f095164f6
unknown <borut60@gmail.com>
2021-08-12 18:29:00 +0200

    Updated dependencies. Added linter. Removed unused files.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
 
 function App() {
   return (
@@ -8,7 +6,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to
+          reload.ssssssssssssssssssssssssssssssssssssssssssssss
         </p>
         <a
           className="App-link"
